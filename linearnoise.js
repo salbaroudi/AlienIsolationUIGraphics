704c4f75a2616a81bda6c2b0bcebd5289b45bb03
salbaroudi <sean.al.baroudi@gmail.com>
2019-06-06 08:42:02 -0400

    Worked out background random noise

--- linearnoise.js
+++ linearnoise.js
@@ -1,13 +1,31 @@
 
 //Basic canvas initialization, and a reference to it.
 var elem = document.getElementById('canvas');
-var params = { width: 800, height: 800 };
+var params = { width: 600, height: 600 };
 var two = new Two(params).appendTo(elem);
 
+//first, lets try a uniform distribution for our bar thickness and
+//spacing. Just use Math.random()
 
-for (i = 0; i < 10; i++) {
-var rect = two.makeRectangle((10 + 20*i), 50, 20, 20);
-rect.fill = 'rgb(10, 10, 10)';
-two.update()
 
+const recWidth = 600;
+const lSideAlign = 0;
+
+var nextpos = 0;
+var x = 0;
+var y = 0;
+var rect = "";
+
+//unbounded loop if your not careful!
+//Looking at the sample images, using a uniform random distribution is good
+//enough!
+//We also can't render every time we make a rectangle.
+//We need a series of objects, and then render in one shot.
+while(nextpos < 600) {
+    x = Math.floor(Math.random()*6);
+    y = Math.floor(Math.random()*11);
+    rect = two.makeRectangle(lSideAlign,nextpos,recWidth,x)
+    rect.fill = 'rgb(10, 10, 10)';
+    two.update()
+    nextpos = nextpos + y + x;
 }
